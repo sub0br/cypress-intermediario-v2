0c32a48051a99947f15d9a58987401d49edc199e
Thiago Fernandes <thiagofernandesas2@bitbucket.org>
2025-07-24 08:10:04 -0300

    login + logout

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    experimentalStudio: true,
     baseUrl: 'http://localhost',
   },
   fixturesFolder: false,
--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -23,3 +23,22 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('login', (
+  user = Cypress.env('user_name'),
+  password = Cypress.env('user_password'),
+) => {
+  const login = () => {
+    cy.visit('/users/sign_in')
+
+    cy.get("[data-qa-selector='login_field']").type(user)
+    cy.get("[data-qa-selector='password_field']").type(password, { log: false }) //log : false esconde o password
+    cy.get("[data-qa-selector='sign_in_button']").click()
+  }
+
+  login()
+})
+
+Cypress.Commands.add('logout', () => {
+    cy.get('.qa-user-avatar').click()
+    cy.contains('Sign out').click()
+})
