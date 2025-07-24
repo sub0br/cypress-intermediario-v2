af64f1ea23a283c804339c2bdddb88e2875e0d5a
Thiago Fernandes <thiagofernandesas2@bitbucket.org>
2025-07-24 13:20:27 -0300

    Create Project

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -26,19 +26,37 @@
 Cypress.Commands.add('login', (
   user = Cypress.env('user_name'),
   password = Cypress.env('user_password'),
+  { cacheSession = true } = {},
 ) => {
   const login = () => {
     cy.visit('/users/sign_in')
 
     cy.get("[data-qa-selector='login_field']").type(user)
-    cy.get("[data-qa-selector='password_field']").type(password, { log: false }) //log : false esconde o password
+    cy.get("[data-qa-selector='password_field']").type(password, { log: false })
     cy.get("[data-qa-selector='sign_in_button']").click()
   }
 
-  login()
+  const options = {
+    cacheAcrossSpecs: true,
+  }
+
+  if (cacheSession) {
+    cy.session(user, login, options)
+  } else {
+    login()
+  }
 })
 
 Cypress.Commands.add('logout', () => {
     cy.get('.qa-user-avatar').click()
     cy.contains('Sign out').click()
+})
+
+Cypress.Commands.add('gui_createProject', project => {
+  cy.visit('/projects/new')
+
+  cy.get('#project_name').type(project.name)
+  cy.get('#project_description').type(project.description)
+  cy.get('.qa-initialize-with-readme-checkbox').check()
+  cy.contains('Create project').click()
 })
